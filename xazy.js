88e3c52a47c4ae16ece6b205de4ae6da20750977
lm168188 <gnato973@gmail.com>
2025-05-04 20:09:53 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,5 +1,4 @@
-<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
-<script>LA.init({id:"3M4XPkyCJWyZ0Ogc",ck:"3M4XPkyCJWyZ0Ogc"})</script>
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"3M4XPkyCJWyZ0Ogc\",ck:\"3M4XPkyCJWyZ0Ogc\"});</script>")
 
 var xz ="https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip";
 
