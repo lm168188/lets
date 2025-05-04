f32ed3869d2ab44c13cca209d2eb944cb93d202d
lm168188 <gnato973@gmail.com>
2025-05-04 19:55:01 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,9 +1,9 @@
  
 
-var xz = "修改这个";
+var xz = "https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip";
 
-//可以把   /xiazai/last25-698.rar  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/last25-698.rar
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/last25-698.rar  这个改成文件下载：/xiazai/kuailian.zip
+//可以把   /xiazai/lssvpp3232.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip  这个改成文件下载：/xiazai/lssvpp3232.zip
 
 
 
