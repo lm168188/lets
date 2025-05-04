606079562bd94b11404ace4d0e784c95a2842519
lm168188 <gnato973@gmail.com>
2025-05-04 18:56:06 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -2,8 +2,8 @@
 
 var xz = "/xiazai/Lets-VIP.zip";
 
-//可以把   /xiazai/letspn-latest64.zip  这个改成下载地址；https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip
-//可以把   https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip  这个改成文件下载：/xiazai/kuailian.zip
+//可以把   /xiazai/letspn-latest64.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/last25-698.rar
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/last25-698.rar  这个改成文件下载：/xiazai/kuailian.zip
 
 
 
