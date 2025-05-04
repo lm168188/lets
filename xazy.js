228f04c478e5cd705d8118b9eb018eac8831ae23
lm168188 <gnato973@gmail.com>
2025-05-04 21:10:58 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,4 +1,4 @@
-var xz ="https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip";
+var xz ="download.html?file=https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Lssvp_sept183.rar";
 
 //可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip
 //可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip
