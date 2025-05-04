fa2766a1fafaabe7abd775e662468c3faffd0e0b
lm168188 <gnato973@gmail.com>
2025-05-04 20:14:14 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,5 +1,3 @@
-<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
-<script>LA.init({id:"3M4XPkyCJWyZ0Ogc",ck:"3M4XPkyCJWyZ0Ogc"})</script>
 var xz ="https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip";
 
 //可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lssvpp3232.zip
