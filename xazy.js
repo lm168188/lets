9050e4c49434182e2902627ae99d2dc6768da174
lm168188 <gnato973@gmail.com>
2025-05-04 19:44:53 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,6 +1,6 @@
  
 
-var xz = "/xiazai/last25-698.rar";
+var xz = "修改这个";
 
 //可以把   /xiazai/last25-698.rar  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/last25-698.rar
 //可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/last25-698.rar  这个改成文件下载：/xiazai/kuailian.zip
