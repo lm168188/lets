a362bdf4e4b06f4d874ab7ba61d8ed95e597fa76
lm168188 <gnato973@gmail.com>
2025-05-06 19:40:50 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,8 +1,7 @@
-var xz ="https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip";
+var xz ="https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip";
 
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/GTD_XP-Win11_PctletvpnPatron_v51611.133.zip
-
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip
 
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
