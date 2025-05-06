00d8d5cb04be44fdb57a6c2e1f84a85452a0285a
lm168188 <gnato973@gmail.com>
2025-05-07 00:32:57 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,8 +1,7 @@
 var xz ="download.html?file=https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip";
 
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip
-
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-x64.14.3.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-x64.14.3.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-x64.14.3.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-x64.14.3.zip
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
     if (links[i].hasAttribute("href")) {
