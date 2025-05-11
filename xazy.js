a61db73a69a3981a59fe2c09cb300776146a78bb
lm168188 <gnato973@gmail.com>
2025-05-12 01:29:07 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,7 +1,7 @@
-var xz ="download.html?file=https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-3.14.3.zip";
+var xz ="download.html?file=https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lesv.15.5.3.zip";
 
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-3.14.3.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-3.14.3.zip
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-3.14.3.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-3.14.3.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lesv.15.5.3.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lesv.15.5.3.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lesv.15.5.3.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lesv.15.5.3.zip
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
     if (links[i].hasAttribute("href")) {
