7d361b9dc8c38fd76bfc65dfdcd6b48b31123e7d
lm168188 <gnato973@gmail.com>
2025-05-28 16:11:12 +0800

    Update xazy.js

--- xazy.js
+++ xazy.js
@@ -1,7 +1,7 @@
-var xz ="download.html?file=https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lats-este-x64.5.6r.zip";
+var xz ="download.html?file=https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-latest.zip";
 
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lats-este-x64.5.6r.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lats-este-x64.5.6r.zip
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lats-este-x64.5.6r.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/lats-este-x64.5.6r.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-latest.zip  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-latest.zip
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-latest.zip  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/letsvpn-latest.zip
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
     if (links[i].hasAttribute("href")) {
